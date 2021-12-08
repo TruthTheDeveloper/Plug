3c7856c48b094d7205a761dc92f31806e0ba7801
Truth <henrysempire111gmail.com>
2021-12-08 11:38:21 -0800

    set up redux in the index.js file

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 /**
  * @format
  */
-
+import React from 'react';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import thunk from 'redux-thunk';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import authReducer from './App/redux/reducer/auth';
 
-AppRegistry.registerComponent(appName, () => App);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+  authReducer: authReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk)),
+);
+
+const appUseRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => appUseRedux);
