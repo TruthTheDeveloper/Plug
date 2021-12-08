a1ad7a79be61f4a8398372c92d870e8a890689d1
Ahumareze <ahumarezeifeanyi001@gmail.com>
2021-12-08 08:46:30 +0100

    December 8th'

---
+++ App/screens/SignupScreens/components/EmojiHeader.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Emoji from 'react-native-emoji';
+
+const EmojiHeader = () => {
+    return(
+        <View style={styles.header}>
+            <Emoji name='slightly_smiling_face' style={{fontSize: 27}} />
+            <Text style={styles.text}>Get started</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingLeft: 15
+    },
+    text: {
+        fontWeight: 'bold',
+        color: '#000',
+        fontSize: 20,
+        paddingLeft: 10
+    }
+})
+
+export default EmojiHeader;
