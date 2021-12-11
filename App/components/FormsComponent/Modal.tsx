dfef1215920d4787c877917aca8eb100330f6546
Ahumareze <ahumarezeifeanyi001@gmail.com>
2021-12-11 10:25:50 +0100

    Decemver 11th

---
+++ App/components/FormsComponent/Modal.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+
+const Modal = () => {
+    return(
+        <View></View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        
+    }
+})
