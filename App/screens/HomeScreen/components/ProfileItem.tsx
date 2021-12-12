9ac2cff5df6354cc1adf4e138a610a4c07cc0f86
Ahumareze <ahumarezeifeanyi001@gmail.com>
2021-12-13 00:04:26 +0100

    December 13th

--- App/screens/HomeScreen/components/ProfileItem.tsx
+++ App/screens/HomeScreen/components/ProfileItem.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, Image } from 'react-native';
 
-const image = require('../../../assets/images/girl.jpg')
+const image = require('../../../assets/images/girl.jpg');
+const verifiedIcon = require('../../../assets/images/verified.png');
 
 const ProfileItem = () => {
     return(
         <View style={styles.container}>
             <View style={styles.main}>
                 <ImageBackground source={image} style={styles.backgroundImage}>
-                    <View style={styles.details}></View>
+                    <View style={styles.details}>
+                        <View style={styles.usernameContainer}>
+                            <Text style={styles.username}>kendallJenner</Text>
+                            <Image source={verifiedIcon} />
+                        </View>
+                        <Text style={styles.department}>400l,Computer scie..</Text>
+                    </View>
                 </ImageBackground>
             </View>
         </View>
@@ -34,11 +41,31 @@ const styles = StyleSheet.create({
         width: '100%',
     },
     details: {
-        height: 70,
         width: '100%',
         backgroundColor: 'rgba(0,0,0,0.5)',
         position: 'absolute',
-        bottom: 0
+        bottom: 0,
+        paddingBottom: 15,
+        paddingTop: 10
+    },
+    usernameContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingLeft: 5
+    },
+    username: {
+        color: '#fff',
+        fontSize: 17,
+        fontWeight: '600',
+        paddingRight: 5
+    },
+    department: {
+        color: '#fff',
+        opacity: 0.7,
+        paddingLeft: 5,
+        fontWeight: '600',
+        fontSize: 16
     }
 });
 
