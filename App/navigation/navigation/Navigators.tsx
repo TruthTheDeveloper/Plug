1528cdb292fe9ac8850e19286268282473f96312
Ahumareze <ahumarezeifeanyi001@gmail.com>
2021-12-13 13:48:24 +0100

    December 13th

--- App/navigation/navigation/Navigators.tsx
+++ App/navigation/navigation/Navigators.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 //components
 import HomeScreenContainer from '../../screens/HomeScreen/index';
 
+
+const Home = () => {
+    return(
+        <View style={{backgroundColor: 'red', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Hello world</Text>
+        </View>
+    )
+}
+
+const Stack = createNativeStackNavigator();
+
 const Navigator = () => {
     return(
         <NavigationContainer>
-            <HomeScreenContainer />
+            <Stack.Navigator>
+                <Stack.Screen name='Home' component={Home} />
+            </Stack.Navigator>
         </NavigationContainer>
     )
 };
