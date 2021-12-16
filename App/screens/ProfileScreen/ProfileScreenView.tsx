386774d58f324639def21e2469cf9c877aa49f50
Ahumareze <ahumarezeifeanyi001@gmail.com>
2021-12-16 12:38:31 +0100

    December 16th

---
+++ App/screens/ProfileScreen/ProfileScreenView.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+
+const ProfileScreenView = () => {
+    return(
+        <View style={styles.container}></View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        
+    }
+});
+
+export default ProfileScreenView
