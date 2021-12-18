526c6723dd35e6b90771463b109d7651602055b5
Ahumareze <ahumarezeifeanyi001@gmail.com>
2021-12-18 13:09:38 +0100

    December 18th

--- App/screens/ProfileScreen/components/ExtraButtons.tsx
+++ App/screens/ProfileScreen/components/ExtraButtons.tsx
@@ -9,10 +9,21 @@ interface ExtraButtonsProps {
     cancle: () => void
 }
 
+const buttonDiv = (width-30)/2
+
 const ExtraButtons:FC<ExtraButtonsProps> = ({submit, cancle}):JSX.Element => {
     return(
         <View style={styles.container}>
-            <View style={styles.div}></View>
+            <View style={styles.div}>
+                <View style={styles.button}>
+                    <Text style={styles.text}>Cancle</Text>
+                </View>
+            </View>
+            <View style={styles.div2}>
+                <View style={[styles.button2, {backgroundColor: '#000'}]}>
+                    <Text style={[styles.text, {color: '#fff'}]}>Update</Text>
+                </View>
+            </View>
         </View>
     )
 };
@@ -21,14 +32,44 @@ const styles = StyleSheet.create({
     container: {
         height: 45,
         width: width - 30,
-        backgroundColor: red,
         marginBottom: 200,
-        marginTop: 40
+        marginTop: 40,
+        marginLeft: 15,
+        display: 'flex',
+        flexDirection: 'row'
     },
     div: {
         height: 45,
-        width: (width-30)/2,
-        backgroundColor: 'blue'
+        width: buttonDiv,
+    },
+    div2: {
+        height: 45,
+        width: buttonDiv,
+        flexDirection: 'row-reverse'
+    },
+    button: {
+        height: 45,
+        width: buttonDiv - 30,
+        backgroundColor: '#E4E4E4',
+        marginLeft: 10,
+        borderRadius: 10,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    button2: {
+        height: 45,
+        width: buttonDiv - 30,
+        backgroundColor: '#E4E4E4',
+        marginRight: 10,
+        borderRadius: 10,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text: {
+        color: '#000',
+        fontSize: 20,
     }
 })
 
