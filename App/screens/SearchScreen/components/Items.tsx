1f2ffdd888931ac4757347db9517b5c5d8894050
Ahumareze <ahumarezeifeanyi001@gmail.com>
2021-12-21 21:46:19 +0100

    December 21th

---
+++ App/screens/SearchScreen/components/Items.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+
+const Item = () => {
+    return(
+        <View></View>
+    )
+};
+
+export default Item;
