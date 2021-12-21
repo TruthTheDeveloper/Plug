800f381d500f14990d49c88567dcfbf1c9f61019
Ahumareze <ahumarezeifeanyi001@gmail.com>
2021-12-21 23:11:07 +0100

    December 21th

--- App/components/MainComponents/Loader.tsx
+++ App/components/MainComponents/Loader.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { View, Text, Dimensions, StyleSheet } from 'react-native';
 
+import LottieView from 'lottie-react-native';
+
+const loader = require('../../assets/others/spinner.json');
+
 const {height, width} = Dimensions.get('window')
 
 const Loader = () => {
     return(
-        <View style={styles.container}></View>
+        <View style={styles.container}>
+            <LottieView
+                source={loader}
+                autoPlay
+                loop
+            />
+        </View>
     )
 };
 
@@ -13,7 +23,7 @@ const styles = StyleSheet.create({
     container: {
         height: height,
         width: width,
-        backgroundColor: 'red'
+        backgroundColor: 'green'
     }
 });
 
