abd5d87cf2d5d4f1b2a734f0e7c8b14c5797a667
Truth <henrysempire111gmail.com>
2021-12-26 12:44:46 -0800

    starting anew

--- App/screens/SignupScreens/components/SignupScreen3.tsx
+++ App/screens/SignupScreens/components/SignupScreen3.tsx
@@ -21,15 +21,10 @@ import ProfilePhoto from './ProfilePhoto';
 import PersonalityBox from './personalityBox';
 import ContinueButton from './ContinueButton';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import io from 'socket.io-client';
-
-
+import uuid from 'react-native-uuid';
 
 const {height, width} = Dimensions.get('window');
-
-const SOCKET_URL = io('https://findplug.herokuapp.com');
-let socket : any;
-let socketId : any;
+let socketId: any;
 const SignupScreen3 = () => {
   const [personality, setPersonality] = useState<any | null>([]);
 
@@ -38,14 +33,9 @@ const SignupScreen3 = () => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    console.log('useEffect', socketId)
-    socket = SOCKET_URL;
-    socket.on('connect', () => {
-        console.log('you are now connected');
-        socketId = socket.id;
-        console.log(socketId, 'socketId');
-    });
+    socketId = uuid.v4();
   },[]);
+
 
   const setImage = (img: any) => {
     setProfilePic(img);
