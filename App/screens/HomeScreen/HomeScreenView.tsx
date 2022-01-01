c5befb99d8985813367de3b14aa21af0d2befe3e
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-01-01 21:58:37 +0100

    January 1th

--- App/screens/HomeScreen/HomeScreenView.tsx
+++ App/screens/HomeScreen/HomeScreenView.tsx
@@ -8,7 +8,7 @@ import * as actions from '../../redux/actions/index';
 import * as actionTypes from '../../redux/actions/actionTypes';
 
 //Components
-import {Header} from '../../components/index';
+import {Header, Loader} from '../../components/index';
 
 import ProfileItem from './components/ProfileItem';
 import Profile from './components/Profile';
@@ -59,15 +59,6 @@ const HomeScreenView:FC<homeProps> = React.memo(({navigate}):JSX.Element => {
       setPageNum(prev => prev + 1);
     },[dispatch, pageNum]);
 
-  const [] = useState([
-    {username: 'kendall_jenner', level: 400, department: 'English', image: girl1, availability: true, details: 'Looking for a sharp looking roomate, one who is NOT A JEW PERSON jkdkd dkmdldd mlsmlss ,m,mdd  kmlms ,dmdld mlmsllsd fdflkdfl flflkf lklf fmllf mflmlf f,mlfm fmlflf flmfmdfl flmdflmdf dfmlmdf fdmdglmdg glmdgldgmm' },
-    {username: 'marysmith', level: 100, department: 'Law', image: girl2, availability: true, details: 'Looking for a roomate who can clean and cook, and also one who is NOT A JEW PERSON'},
-    {username: 'clarris', level: 100, department: 'Chemistry', image: girl3, availability: true, details: 'Looking for a sharp looking roomate, one who is NOT A JEW PERSON'},
-    {username: 'officialSasha', level: 200, department: 'Computer Science', image: girl4, availability: true, details: 'Looking for a roomate who can clean and cook, and also one who is NOT A JEW PERSON'},
-    {username: 'poppins', level: 100, department: 'Geography', image: girl5, availability: true, details: 'Looking for a sharp looking roomate, one who is NOT A JEW PERSON'},
-    {username: 'queenjanedoe', level: 300, department: 'Statistics', image: girl6, availability: true, details: 'Looking for a roomate who can clean and cook, and also one who is NOT A JEW PERSON'},
-  ]);
-
   const goBack = () => {
     dispatch({type: actionTypes.SHOW_CARDS, value: false});
     return true;
@@ -84,23 +75,27 @@ const HomeScreenView:FC<homeProps> = React.memo(({navigate}):JSX.Element => {
   return (
     <View>
       <Header label="All Student" />
-      <FlatList
-            key={'_'}
-            numColumns={2}
-            data={profileData}
-            renderItem={({ item, index }) => <ProfileItem
-              username={item.username}
-              verified={item.availability}
-              level={item.level}
-              department={item.department}
-              image={item.profilePic}
-              index={index}
-              setIndex={openGrid}
-              />
-            }
-            style={{ marginBottom: 37 }}
-            onEndReached={getNewList}
+      {profileData ?
+        <FlatList
+          key={'_'}
+          numColumns={2}
+          data={profileData}
+          renderItem={({ item, index }) => <ProfileItem
+            username={item.username}
+            verified={item.availability}
+            level={item.level}
+            department={item.department}
+            image={item.profilePic}
+            index={index}
+            setIndex={openGrid}
             />
+          }
+          style={{ marginBottom: 37 }}
+          onEndReached={getNewList}
+        />
+      :
+        <Loader />
+      }
     </View>
   );
 });
