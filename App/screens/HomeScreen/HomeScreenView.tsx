ca64442f3d055b0cd55896dd6ea2fed18a516838
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-01-04 02:10:05 +0100

    January 4th

--- App/screens/HomeScreen/HomeScreenView.tsx
+++ App/screens/HomeScreen/HomeScreenView.tsx
@@ -17,6 +17,7 @@ import ProfileItem from './components/ProfileItem';
 // import DetailsDiv from './components/DetailsDiv';
 
 
+
 //Imported Images
 const girl1 = require('../../assets/images/girl.jpg');
 const girl2 = require('../../assets/images/girl1.jpg');
